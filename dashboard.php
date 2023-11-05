9104999d2488082795e583535309993c6b29b36d
xxIto <christofernero@gmail.com>
2023-11-05 20:43:43 +0900

    ALOO

---
+++ dashboard.php
@@ -0,0 +1,41 @@
+ <!-- Content Wrapper. Contains page content -->
+ <div class="content-wrapper">
+
+     <br />
+
+     <!-- Main content -->
+     <div class="content">
+         <div class="container-fluid">
+             <div class="row">
+                 <div class="col-lg-6">
+                     <div class="card">
+                         <div class="card-body">
+                             <h5 class="card-title">Tugas CRUD</h5>
+
+                             <p class="card-text">
+                             <table>
+                                 <tr>
+                                     <td>Nama</td>
+                                     <td>: Fransiskus Xaverius Kanembut</td>
+                                 </tr>
+                                 <tr>
+                                     <td>NPM:</td>
+                                     <td>: 202057201048</td>
+                                 </tr>
+                                 <tr>
+                                     <td>Prodi</td>
+                                     <td>: Sistem Informasi</td>
+                                 </tr>
+                             </table>
+                             </p>
+                         </div>
+                     </div>
+
+
+                 </div>
+
+             </div>
+
+         </div>
+     </div>
+ </div>
